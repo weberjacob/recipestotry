d79c818d867238cc3b7062e0bd9cc9f4df582c37
jacobweber <jacobaweber@gmail.com>
2019-06-29 06:33:01 -0500

    Adding a little sorting and organization

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,24 +8,79 @@ import SEO from "../components/seo"
 
 class IndexPage extends React.Component {
   render() {
-    const categories = get(this, 'props.data.allGoogleSheetSheet1Row.distinct')
-    const categoryList = categories.map((category) =>
-      <div key={category.toString()}>
-        { category }
-      </div>
-    );
+    const categories = get(this, 'props.data.all.distinct')
+    const breakfast = get(this, 'props.data.breakfast.edges')
+    const dessert = get(this, 'props.data.dessert.edges')
+    const main = get(this, 'props.data.main.edges')
+    const pasta = get(this, 'props.data.pasta.edges')
+    const salad = get(this, 'props.data.salad.edges')
+    const side = get(this, 'props.data.side.edges')
 
-    const recipes = get(this, 'props.data.allGoogleSheetSheet1Row.edges')
-    // console.log(category);
     return (
       <Layout>
         <SEO title="Home" />
         <h1>Try something new</h1>
 
-        {/* { categoryList } */}
-          
+        {/* { categories } */}
+
+        <a name="breakfast"><h2>🍳 Breakfast 🥓</h2></a>
         <ul className="recipe-list">
-          { recipes.map(({ node }) => {
+          { breakfast.map(({ node }) => {
+            return (
+              <li key={node.id}>
+                <a href={node.url} target="_blank" rel="noopener noreferrer">{node.title}</a>
+              </li>
+            )
+          })}
+        </ul>
+
+        <h2>🍨 Dessert 🍦</h2>
+        <ul className="recipe-list">
+          { dessert.map(({ node }) => {
+            return (
+              <li key={node.id}>
+                <a href={node.url} target="_blank" rel="noopener noreferrer">{node.title}</a>
+              </li>
+            )
+          })}
+        </ul>
+
+        <h2>🍗 Main Dish 🐄</h2>
+        <ul className="recipe-list">
+          { main.map(({ node }) => {
+            return (
+              <li key={node.id}>
+                <a href={node.url} target="_blank" rel="noopener noreferrer">{node.title}</a>
+              </li>
+            )
+          })}
+        </ul>
+
+        <h2>🍝 Pasta 🍝 </h2>
+        <ul className="recipe-list">
+          { pasta.map(({ node }) => {
+            return (
+              <li key={node.id}>
+                <a href={node.url} target="_blank" rel="noopener noreferrer">{node.title}</a>
+              </li>
+            )
+          })}
+        </ul>
+        
+        <h2>🥬  Salad 🥗 </h2>
+        <ul className="recipe-list">
+          { salad.map(({ node }) => {
+            return (
+              <li key={node.id}>
+                <a href={node.url} target="_blank" rel="noopener noreferrer">{node.title}</a>
+              </li>
+            )
+          })}
+        </ul>
+
+        <h2>🌽Side 🍟</h2>
+        <ul className="recipe-list">
+          { side.map(({ node }) => {
             return (
               <li key={node.id}>
                 <a href={node.url} target="_blank" rel="noopener noreferrer">{node.title}</a>
@@ -41,8 +96,8 @@ class IndexPage extends React.Component {
 export default IndexPage
 
 export const recipeQuery = graphql`
-  query RecipeIndexQuery {
-    allGoogleSheetSheet1Row(sort: {fields: category}) {
+  query getRecipes {
+    all: allGoogleSheetSheet1Row(sort: {fields: [title]}) {
       edges {
         node {
           id
@@ -52,6 +107,66 @@ export const recipeQuery = graphql`
         }
       }
       distinct(field: category)
+    },
+    breakfast: allGoogleSheetSheet1Row(sort: {fields: [title]}, filter: {category: {eq: "breakfast"}}) {
+      edges {
+        node {
+          id
+          title
+          url
+          category
+        }
+      }
+    },
+    dessert: allGoogleSheetSheet1Row(sort: {fields: [title]}, filter: {category: {eq: "dessert"}}) {
+      edges {
+        node {
+          id
+          title
+          url
+          category
+        }
+      }
+    },
+    main: allGoogleSheetSheet1Row(sort: {fields: [title]}, filter: {category: {eq: "main"}}) {
+      edges {
+        node {
+          id
+          title
+          url
+          category
+        }
+      }
+    },
+    pasta: allGoogleSheetSheet1Row(sort: {fields: [title]}, filter: {category: {eq: "pasta"}}) {
+      edges {
+        node {
+          id
+          title
+          url
+          category
+        }
+      }
+    },
+    salad: allGoogleSheetSheet1Row(sort: {fields: [title]}, filter: {category: {eq: "salad"}}) {
+      edges {
+        node {
+          id
+          title
+          url
+          category
+        }
+      }
+    },
+    side: allGoogleSheetSheet1Row(sort: {fields: [title]}, filter: {category: {eq: "side"}}) {
+      edges {
+        node {
+          id
+          title
+          url
+          category
+        }
+      }
     }
   }
 `
